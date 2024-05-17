3ad9a7259b2e8cc6f507ffefbc3357c8f2623fe0
EduwardSimanjuntak123 <149449338+EduwardSimanjuntak123@users.noreply.github.com>
2024-05-17 19:40:35 +0700

    first

---
+++ spempat/resources/views/layouts_user/main.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/' . $halaman . '.css') }}">
+    <link rel="shortcut icon" href="{{ asset('img/Tutwuri.png') }}" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaisei+Tokumin&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>SMP4BLG || {{ $halaman }}</title>
+    <style>
+        @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
+    </style>
+</head>
+
+<body style="">
+    <!--header-->
+    @include('layouts_user.header')
+
+
+    <!--kata sambutan-->
+    @yield('content')
+
+    <!--Informasi/Kontak-->
+
+    @include('layouts_user.footer')
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="{{ asset('js/js.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
